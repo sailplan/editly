bfbf24e04d6c120625e2512e3462e8ca77cc43e3
Brandon Keepers <brandon@opensoul.org>
2025-01-10 17:07:10 -0500

    Run all examples in run-all-examples.sh

--- examples/run-all-examples.sh
+++ examples/run-all-examples.sh
@@ -1,13 +1,10 @@
 #/bin/bash
-set -e
+set -xe
 
-node ../cli.js --json gl.json5
-node ../cli.js --json image.json5
-node ../cli.js --json losslesscut.json5
-node ../cli.js --json resizeHorizontal.json5
-node ../cli.js --json resizeVertical.json5
-node ../cli.js --json speedTest.json5
-node ../cli.js --json subtitle.json5
-node ../cli.js --json transitionEasing.json5
-node ../cli.js --json transparentGradient.json5
-node ../cli.js --json commonFeatures.json5
+for f in *.json5; do
+  node ../cli.js --fast --json "$f"
+done
+
+for f in *.js; do
+  node "$f"
+done
